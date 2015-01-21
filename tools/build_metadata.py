d8aa868823e8bbe5f5d345fb649af2f5052fa982
Caleb Hattingh <caleb.hattingh@gmail.com>
2015-01-22 00:00:05 +1000

    Improved the generator/processor of the notebooks.

--- tools/build_metadata.py
+++ tools/build_metadata.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 import os
 from os.path import join, getctime, splitext, basename
-import sys
 import begin
 import glob
 import time
@@ -15,9 +14,10 @@ def process(notebook_folder, output_folder):
     the output folder. Metadata json files will also be created and
     will contain items like the creation date of each notebook file."""
     metadata = {}
+    # Walk over all the IPython Notebook files
     for nbfile in glob.glob(join(notebook_folder, '*.ipynb')):
-        timestamp = getctime(nbfile)
-        creation_date = time.ctime(getctime(nbfile))
+        timestamp = getctime(nbfile)  # Get the notebook creation time
+        creation_date = time.ctime(getctime(nbfile))  # as a string
         nbbasefile = basename(nbfile)
         print 'Processing "{}"...'.format(nbbasefile)
         postname, ext = splitext(nbbasefile)
@@ -25,11 +25,13 @@ def process(notebook_folder, output_folder):
                 timestamp=timestamp,
                 creation_date=creation_date
                 )
+        # Convert the notebook file to plain HTML, add to posts/ folder
         tmpl = 'ipython nbconvert --to html --template basic "{}" --stdout > "{}"'
         os.system(tmpl.format(nbfile, join(output_folder, postname+'.html')))
         print creation_date
 
     # Move through the list, building up the cross links
+    # Posts are sorted by creation timestamp
     sorted_posts = list(sorted(metadata, key=lambda x: metadata[x]['timestamp']))
     for i, postname in enumerate(sorted_posts):
         if i == 0:
@@ -39,6 +41,7 @@ def process(notebook_folder, output_folder):
 
         if i == len(sorted_posts) - 1:
             next_post = None
+            last_post = postname
         else:
             next_post = sorted_posts[i + 1]
 
@@ -48,6 +51,8 @@ def process(notebook_folder, output_folder):
         print postname
         with open(join(output_folder, postname+'.json'), 'w') as f:
             json.dump(metadata[postname], f, indent=4)
+    with open(join(output_folder, 'latest.json'), 'w') as f:
+        json.dump(dict(post=last_post), f, indent=4)
 
 
 
